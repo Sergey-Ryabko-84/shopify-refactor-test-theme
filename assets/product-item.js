df69867617ee25b13ea03d6898802bb853f1819c
serhii.ryabko <serhii.ryabko@gmail.com>
2025-03-12 21:26:52 +0200

    Add product-item-form snippet and integrate add-to-cart functionality

--- assets/product-item.js
+++ assets/product-item.js
@@ -12,7 +12,7 @@ class ProductItem extends HTMLElement {
       <div class="product-item">
         <img class="product-item__image" src="${image}" alt="${title}" />
         <h3 class="product-item__title">${title}</h3>
-        <button class="product-item__submit_button">
+        <button class="product-item__submit_button" type="submit">
           Add to cart - ${price}
         </button>
       </div>
