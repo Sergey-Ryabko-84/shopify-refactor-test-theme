2cf22d9f4d19d7cc323cbf2da3232d7a9b1a9de0
serhii.ryabko <serhii.ryabko@gmail.com>
2025-03-12 19:50:10 +0200

    Add product-item

---
+++ assets/product-item.js
@@ -0,0 +1,23 @@
+class ProductItem extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const title = this.getAttribute('title');
+    const image = this.getAttribute('image');
+    const price = this.getAttribute('price');
+
+    this.innerHTML = `
+      <div class="product-item">
+        <img class="product-item__image" src="${image}" alt="${title}" />
+        <h3 class="product-item__title">${title}</h3>
+        <button class="product-item__submit_button">
+          Add to cart - ${price}
+        </button>
+      </div>
+    `;
+  }
+}
+
+customElements.define('product-item', ProductItem);
